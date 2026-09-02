c81abc93a2d86743381fb29b66f99c9182515b68
omegacrow3 <omegacrow333@gmail.com>
2026-09-02 22:08:24 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,25 @@
+'''My loveliest prettiest gorgeousest perfectest babiii
+where do I even beginnnn'''
+
+'''My boobewwy muffin, beeeeg girl now 16 anol
+ehehehe YIPPEEEEEE'''
+
+'''You make the world a better place
+just by existing. You're literally my
+everything and I'm beyond grateful to have
+met you ml'''
+
+'''You're too pure for this world, the beegest
+bundle of sunshine rainbows and lollipops <333'''
+
+'''Let's hope this year is a smooooth operatiooooon'''
+
+'''From the bottom of my heart mi amor, HAPPY BIRTHDAY!!!
+I HOPE THIS YO BEST BUDDAY TILL DATE AND YOU HAVE THE
+MOST AWESOMEST MOST BESTEST OF DAYS MY LOVEEEE'''
+
+'''I'd say so much more ml but yeahh
+HAPPYYYYY 16TH BIRTHDAYYYY SWEET 16 RAHHHHH
+MY BEEEG GORL GAWJUS PERFECT LADYYYY
+ILY MORE THAN ANYTHING AND EVERYTHING HMPH
+HAPPYYYYY BIRTHDAYYY <33333333333333333'''
